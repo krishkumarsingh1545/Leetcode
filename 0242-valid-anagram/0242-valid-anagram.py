342b139ee90e01a96f96f7bdcba0229a680afb2c
Krish Kumar Singh <105716247+krishkumarsingh1545@users.noreply.github.com>
2026-09-24 12:27:36 +0530

    Time: 15 ms (43.11%), Space: 19.4 MB (76.69%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        d1, d2 = {}, {}
+        for i in range(len(s)):
+            if s[i] in d1:
+                d1[s[i]] += 1
+            else:
+                d1[s[i]] = 1
+            if t[i] in d2:
+                d2[t[i]] += 1
+            else:
+                d2[t[i]] = 1
+        return d1 == d2
